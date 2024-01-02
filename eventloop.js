c302489baab17ee926bbbe80d2239ff77f218568
josecobi <58313777+josecobi@users.noreply.github.com>
2024-01-02 12:44:56 -0500

    created recursive function to flatten arrays

--- eventloop.js
+++ eventloop.js
@@ -1,13 +1,36 @@
-let globalCounter = 0;
+//>>>>>>>>>>>>>>>>>>>>>>>>Part 1: Stack overflow<<<<<<<<<<<<<<<<<<<
+// let globalCounter = 0;
 
-function increment() {
-  globalCounter++;
-  increment();
+// function increment() {
+//   globalCounter++;
+//   increment();
+// }
+
+// try {
+//     increment();
+// } catch (error) {
+//   console.error(`Error: ${error.message}`);
+//   console.log(`Global counter: ${globalCounter}`);
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>part 2: Trampolines<<<<<<<<<<<<<<<
+let nestedArray = [1, [2, [3, 4], 5], [6, 7], 8, [9, [10, 11]]];
+function flattenArray(arr){
+  
+  arr.forEach((element) => {
+    if(Array.isArray(element)){
+      flattenArray(element);
+      
+    }
+    else {
+      newArr.push(element);      
+    }
+  });
+  
 }
 
-try {
-    increment();
-} catch (error) {
-  console.error(`Error: ${error.message}`);
-  console.log(`Global counter: ${globalCounter}`);
-}
+let newArr = [];
+flattenArray(nestedArray);
+console.log(newArr);
+
