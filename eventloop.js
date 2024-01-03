17c91380670dd9a9b28f22a425491e8782bc78b0
josecobi <58313777+josecobi@users.noreply.github.com>
2024-01-02 19:07:05 -0500

    Created index.html

--- eventloop.js
+++ eventloop.js
@@ -15,33 +15,34 @@
 
 
 //>>>>>>>>>>>>>>>>>>>>part 2: Trampolines<<<<<<<<<<<<<<<
-let nestedArray = [1, [2, [3, 4], 5], [6, 7], 8, [9, [10, 11]]];
+// let nestedArray = [1, [2, [3, 4], 5], [6, 7], 8, [9, [10, 11]]];
 
-function trampoline(fn) {
-    return function (...args) {
-        let result = fn(...args);
-        while (typeof result === 'function') {
-            result = result();
-        }
-        return result;
-    };
-}
+// function trampoline(fn) {
+//     return function (...args) {
+//         let result = fn(...args);
+//         while (typeof result === 'function') {
+//             result = result();
+//         }
+//         return result;
+//     };
+// }
 
-function flattenArray(arr) {
-    function flatten(arr, result = []) {
-        arr.forEach((element) => {
-            if (Array.isArray(element)) {
-                return flatten(element, result);
-            } else {
-                result.push(element);
-            }
-        });
-        return result;
-    }
+// function flattenArray(arr) {
+//     function flatten(arr, result = []) {
+//         arr.forEach((element) => {
+//             if (Array.isArray(element)) {
+//                 return flatten(element, result);
+//             } else {
+//                 result.push(element);
+//             }
+//         });
+//         return result;
+//     }
 
-    return trampoline(flatten)(arr);
-}
+//     return trampoline(flatten)(arr);
+// }
 
-let result = flattenArray(nestedArray);
-console.log(result);
+// let result = flattenArray(nestedArray);
+// console.log(result);
 
+//>>>>>>>>>>>>>>>>>>>>part 3: Trampolines<<<<<<<<<<<<<<<
