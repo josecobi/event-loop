780439c450e60435aa761a3c0544900936b07089
josecobi <58313777+josecobi@users.noreply.github.com>
2024-01-02 23:37:01 -0500

    function to list prime nummbers with recursion and timeout

--- eventloop.js
+++ eventloop.js
@@ -49,25 +49,33 @@
 
 const paragraphEl = document.querySelector(".paragraph");
 
-function listPrimeNumbers(n){
-    for(let i = 0; i < n; i++){
-        if(isPrime(i)){
-            paragraphEl.textContent += ` ${i}`;
+function listPrimeNumbersDeferred(n, currentNumber = 0) {
+    if (currentNumber <= n) {
+        if (isPrime(currentNumber)) {
+            paragraphEl.textContent += ` ${currentNumber}`;
         }
-    }    
-    alert("Calculation is finished");
+
+    
+        setTimeout(() => {
+            listPrimeNumbersDeferred(n, currentNumber + 1);
+        }, 0);
+    } 
+    else {
+       
+        alert("Calculation is finished");
+    }
 }
 
-function isPrime(number){
-    if(number < 2){
+function isPrime(number) {
+    if (number < 2) {
         return false;
     }
-    for (let i = 2; i <= Math.sqrt(number); i++){
-        if(number % i === 0){
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
             return false;
         }
     }
     return true;
 }
 
-listPrimeNumbers(10000);
+listPrimeNumbersDeferred(10000);
